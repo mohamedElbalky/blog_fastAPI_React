e9c9e4994fdc5fe0c4fae998f60bfef134c4b845
mohamedElbalky <mohammed4elbalky@gmail.com>
2024-05-08 14:57:37 +0300

    setup database

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
 
+from database import models
+from database.db_settings import engine, get_db
+
+
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
 @app.get('/')
 def index():
---
+++ src/routers/schemas.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+
+class PostBase(BaseModel):
+    image_url: str
+    title: str
+    content: str
+    creator: str
+    
+    
+class PostDisplay(PostBase):
+    id = int
+    timestamp: datetime
+    
+    class Config():
+        orm_mode = True
+        
+
